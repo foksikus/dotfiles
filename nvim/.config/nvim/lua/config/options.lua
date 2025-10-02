629bd1e94f7a3acd7c5726f523b390a83920c1fa
foksikus <ed.ulrich238@gmail.com>
2025-10-02 22:32:46 +0200

    Refine Neovim options for indentation and scrolling
    Updated indentation settings and improved comments for clarity.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -16,19 +16,20 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 4 -- amount to indent with << and >>
-vim.opt.tabstop = 4
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
+vim.opt.tabstop = 4 -- How many spaces are shown per Tab
+vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
 
 vim.opt.smarttab = true
 vim.opt.smartindent = true
-vim.opt.autoindent = true
+vim.opt.autoindent = true -- Keep identation from previous line
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
 
 
 -- Save undo history
